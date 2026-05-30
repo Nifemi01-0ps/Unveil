58349864e3d4cf97025c8ebdcbde032788725915
Nifemi01 <raphaeledafesnr@gmail.com>
2026-05-30 17:22:19 +0100

    Add a Mockstory to the mockStories Array

---
+++ src/data/mockStories.js
@@ -0,0 +1,32 @@
+import landScapeImg from "../assets/art01.avif";
+export const mockStories = [
+    {
+        id: 'artwork-01',
+        title: 'Echoes of the High Weald',
+        artist: "Evelyn Sterling",
+        category: "Landscape Expressionism",
+        imageUrl: landScapeImg,
+        artworkAlt: "An abstract Landscape expressionist land scape",
+        hook: 'Nature can be so peaceful without polluction',
+        chapters: [
+            {
+                id: 1,
+                title: "The Moment Before",
+                subtitle: "Inspiration and Origins",
+                content: "In November 2025, the frost on the High Weald did not melt for 3 Months straight. I woke up at 4:00am every single morning to sit in absolute darkness. The silence wasn't peaceful; it felt heavy, almost Suffocating. My hands were freezing, stiff, refusing to hold a brush. I realized that morning that I wasn't trying to paint the landscape outside my window—I was trying to capture the feeling of waiting for an engine to spark to life in the dead of winter."
+            },
+            {
+                id: 2,
+                title: "The Moment Before",
+                subtitle: "Inspiration and Origins",
+                content: "On the fourth afternoon of working on this specific canvas, the composition collapsed entirely. The blues turned muddy, losing all of their translucent light. Out of raw frustration, I took a palette knife and slashed a clean, three-inch tear right through the upper quadrant of the raw cotton warp. I sat on the floor of the studio for four hours doing absolutely nothing. The fix wasn't to patch it up; the fix was to sew it back together with thick, coarse golden twine, letting the scar become the structural baseline for the light layers to rest on."
+            },
+            {
+                id: 3,
+                title: "What I want You to Carry on",
+                subtitle: "Meaning & Reflection",
+                content: "When you look at this piece, do not look for a perfect horizon. Look for the points where the texture breaks. I want you to remember that the moments where your internal map falls completely to pieces are not errors. The tear in your fabric is precisely where the weight of your history begins to look beautiful under a localized spotlight."
+            }
+        ]
+    }
+];
