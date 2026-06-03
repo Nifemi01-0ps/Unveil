06e683a4e90f72cd419d8876fdd9f80020d843ff
Nifemi01 <raphaeledafesnr@gmail.com>
2026-06-03 20:30:40 +0100

    update view path

--- src/routes.jsx
+++ src/routes.jsx
@@ -3,7 +3,7 @@ import AppLayout from "../src/componets/Layout/AppLayout.jsx";
 import LandingPage from "../src/componets/views/LandingPage.jsx";
 import Onboarding from "../src/componets/views/onboarding.jsx";
 import DiscoveryFeed from "../src/componets/Discovery/DiscoveryFeed.jsx";
-import CollectorLibrary from "../src/componets/Views/CollectorLibrary.jsx";
+import CollectorLibrary from "../src/componets/views/CollectorLibrary.jsx";
 import ChapterViewer from "../src/componets/Story/ChapterViewer.jsx";
 import NotFound from "../src/componets/views/NotFound.jsx";
 export const createProjectRouter = ({ stories, ownedIds, handleRegisterPurchase }) => {
