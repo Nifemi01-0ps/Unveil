d50f87cfeda760557d3779f7763df1f6ee8189f2
Nifemi01 <raphaeledafesnr@gmail.com>
2026-06-03 19:30:36 +0100

    add Header component to mobileContainer so as to reflect in all pages

--- src/componets/Layout/AppLayout.jsx
+++ src/componets/Layout/AppLayout.jsx
@@ -3,16 +3,14 @@ import MobileContainer from "../Layout/MobileContainer.jsx";
 import MainContent from "../Layout/MainContent.jsx";
 import Header from "../Common/Header.jsx";
 
-export default function AppLayout() {
+export default function AppLayout({ ownedCount }) {
     const location = useLocation();
     // Custom tracking logic
     const isStoryView = location.pathname.startsWith('/story');
     return (
         <MobileContainer breakpoint={isStoryView ? 'tabletAdapt' : 'default'}>
-            {location.pathname !== '/' && location.pathname !== '/onboarding' && (
-                <Header currentView={location.pathname}/>
-            )}
-            <MainContent id="main-content-anchor">
+            <Header ownedCount={ownedCount}></Header>
+            <MainContent id="main-content-anchor" role='main'>
                 <Outlet />
             </MainContent>
         </MobileContainer>
