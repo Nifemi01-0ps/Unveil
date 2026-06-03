4abf013cfbf689e955df3f1a6e4517cbd3505b61
Nifemi01 <raphaeledafesnr@gmail.com>
2026-06-03 20:24:42 +0100

    correct my views pathway

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,11 +1,11 @@
 import { createBrowserRouter } from "react-router";
 import AppLayout from "../src/componets/Layout/AppLayout.jsx";
-import LandingPage from "../src/componets/Views/LandingPage.jsx";
-import Onboarding from "../src/componets/Views/onboarding.jsx";
+import LandingPage from "../src/componets/views/LandingPage.jsx";
+import Onboarding from "../src/componets/views/onboarding.jsx";
 import DiscoveryFeed from "../src/componets/Discovery/DiscoveryFeed.jsx";
 import CollectorLibrary from "../src/componets/Views/CollectorLibrary.jsx";
 import ChapterViewer from "../src/componets/Story/ChapterViewer.jsx";
-import NotFound from "../src/componets/Views/NotFound.jsx";
+import NotFound from "../src/componets/views/NotFound.jsx";
 export const createProjectRouter = ({ stories, ownedIds, handleRegisterPurchase }) => {
   return createBrowserRouter([
     {
